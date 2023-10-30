318c5e7899d8fdaa9e8cfcea399739413dba7779
Jalen <139247739+Jmmcclo2023@users.noreply.github.com>
2023-10-29 23:27:10 -0400

    Update ThreeStuff.js
    (Still testing the EC2 Deploy workflow)

--- ThreeStuff.js
+++ ThreeStuff.js
@@ -2,6 +2,7 @@ import * as THREE from './node_modules/three/build/three.module.js';
 
 // Testing to see if the EC2 Deploy works
 // Testing Again
+// Third times the charm :/
 
 // SET UP
 // Need a scene, a camera, and a renderer
